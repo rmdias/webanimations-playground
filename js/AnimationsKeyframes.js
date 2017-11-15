99a68e2b9344d115e2bfb07601e37032ccc5e916
Rodolfo Dias <rmdias29@gmail.com>
2017-11-15 16:50:41 +0100

    feat(AnimationsKeyframes): add constant to save all the animations keyframes

---
+++ js/AnimationsKeyframes.js
@@ -0,0 +1,28 @@
+const AnimationsKeyframes = {
+  tailShakeKeyframes: [
+    {transform: 'rotate(0deg)'},
+    {transform: 'rotate(20deg)'},
+    {transform: 'rotate(0deg)'}
+  ],
+  wakeupKeyframes: [
+    {opacity: '0'},
+    {opacity: '1'}
+  ],
+  sleepKeyframes: [
+    {opacity: '1'},
+    {opacity: '0'}
+  ],
+  cloudXKeyframes: [
+    {transform: 'translateX(0)'},
+    {transform: 'translateX(95%)'}
+  ],
+  cloudYKeyframes: [
+    {marginTop: 0},
+    {marginTop: '2px'},
+    {marginTop: 0} 
+  ],
+  cloud2XKeyframes: [
+    {transform: 'translateX(95%)'},
+    {transform: 'translateX(0)'}
+  ]
+};
