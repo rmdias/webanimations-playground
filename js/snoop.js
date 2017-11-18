b8466a57d6c3b3e12391c2e5dcca1315fdde9b6f
Rodolfo Dias <rmdias29@gmail.com>
2017-11-18 19:32:00 +0100

    feat(snoop): snoop sleeping animation

--- js/snoop.js
+++ js/snoop.js
@@ -9,77 +9,12 @@ let tailShakeOptions = {
 
 let snoopTailAnimation = snoopTail.animate(AnimationsKeyframes.tailShakeKeyframes, tailShakeOptions);
 
-
 // sleep
 let sleepyEyes = document.querySelector('#sleepy-eyes');
 let awakeEyes = document.querySelector('#awake-eyes');
 
-let dogSleepBtn = document.querySelector('#dog-sleep');
-let doWakeupBtn = document.querySelector('#dog-wakeup');
-
-dogSleepBtn.addEventListener('click', snoopSleepingAnimation);
-doWakeupBtn.addEventListener('click', snoopWakeupAnimation);
+let snoopSleepBtn = document.querySelector('#dog-sleep');
 
 
-// clouds
-let cloud1 = document.querySelector('#dog-cloud1');
-let cloud2 = document.querySelector('#dog-cloud2');
+snoopSleepBtn.addEventListener('click', snoopSleepingAnimation);
 
-let cloudAnimationX = cloud1.animate(AnimationsKeyframes.cloudXKeyframes,{
-  duration: 20000,
-  fill: 'forwards'  
-});
-cloudAnimationX.onfinish = () => cloudAnimationX.reverse();
-
-let cloudAnimationY = cloud1.animate(AnimationsKeyframes.cloudYKeyframes, {
-  duration: 2000,
-  iterations: Infinity
-});
-
-let cloud2AnimationX = cloud2.animate(AnimationsKeyframes.cloud2XKeyframes,{
-  duration: 20000,
-  fill: 'forwards' 
-});
-cloud2AnimationX.onfinish = () => cloud2AnimationX.reverse();
-
-
-
-
-
-
-
-function isSnoopAwake() {
-  return snoopTailAnimation.playState !== 'paused';
-}
-
-function snoopSleepingAnimation() {
-  if (isSnoopAwake()) {
-    sleepyEyes.animate(AnimationsKeyframes.wakeupKeyframes, {
-      duration: 500,
-      fill: 'forwards'
-    });
-
-    awakeEyes.animate(AnimationsKeyframes.sleepKeyframes, {
-      duration: 500,
-      fill: 'forwards',
-    });
-
-    snoopTailAnimation.pause();
-  }
-};
-
-function snoopWakeupAnimation() {
-  if (!isSnoopAwake()) {  
-    sleepyEyes.animate(AnimationsKeyframes.sleepKeyframes, {
-      duration: 500,
-      fill: 'forwards'
-    });
-
-    awakeEyes.animate(AnimationsKeyframes.wakeupKeyframes, {
-      duration: 500,
-      fill: 'forwards',
-    });
-    
-    snoopTailAnimation.play();
-  }
-};
