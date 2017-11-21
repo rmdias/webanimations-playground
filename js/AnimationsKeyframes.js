a1cfbcdf13d443c95f99536a814604bbb03245b9
Rodolfo Dias <rmdias29@gmail.com>
2017-11-21 21:12:00 +0100

    chore(fish): create `FishKeyframes`

--- js/AnimationsKeyframes.js
+++ js/AnimationsKeyframes.js
@@ -24,5 +24,30 @@ const AnimationsKeyframes = {
   cloud2XKeyframes: [
     {transform: 'translateX(95%)'},
     {transform: 'translateX(0)'}
-  ]
+  ],
+  FishKeyframes: {
+    swimingYmoves: [
+      {transform: 'rotate(-20deg)', marginTop: '-2px'},
+      {transform: 'rotate(20deg)', marginTop: '2px'},
+      {transform: 'rotate(-20deg)', marginTop: '-2px'}
+    ],
+    fish1SwimingXmoves: [
+      {marginLeft: '-20%'},
+      {marginLeft: '100%'}
+    ],
+    fish2SwimingXmoves: [
+      {marginRight: '-20%'},
+      {marginRight: '100%'}
+    ],
+    seaYKeyframes: [
+      {transform: 'translateY(-2px)'},
+      {transform: 'translateY(2px)'},
+      {transform: 'translateY(-2px)'}
+    ],
+    seaXKeyframes: [
+      {backgroundPositionX: '5px'},
+      {backgroundPositionX: '0'},
+      {backgroundPositionX: '5px'}
+    ]
+  }
 };
