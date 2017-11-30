cd33d78fce3309e58ae99651952bf098a561d210
Rodolfo Dias <rmdias29@gmail.com>
2017-11-30 02:15:00 +0100

    feat(snoop): change animations

--- js/snoop.js
+++ js/snoop.js
@@ -1,5 +1,4 @@
 const snoopTheDog = document.querySelector('#snoop');
-var snoopAwake = true;
 
 let snoopTail = document.querySelector('#tail');
 let tailShakeOptions = {
@@ -7,70 +6,88 @@ let tailShakeOptions = {
   iterations: Infinity
 };
 
-let snoopTailAnimation = snoopTail.animate(AnimationsKeyframes.tailShakeKeyframes, tailShakeOptions);
+let snoopTailAnimation = snoopTail.animate([
+  { transform: 'rotate(0deg)' },
+  { transform: 'rotate(20deg)' },
+  { transform: 'rotate(0deg)' }
+ ], tailShakeOptions);
 
 // sleep
 let sleepyEyes = document.querySelector('#sleepy-eyes');
 let awakeEyes = document.querySelector('#awake-eyes');
 
-let snoopSleepBtn = document.querySelector('#dog-sleep');
-let snoopWakeupBtn = document.querySelector('#dog-wakeup');
+let snoopHead = document.querySelector('#head');
 
-snoopSleepBtn.addEventListener('click', snoopSleepingAnimation);
-snoopWakeupBtn.addEventListener('click', snoopWakeupAnimation);
+snoopHead.addEventListener('click', handleSnoopRest);
 
 // clouds
 let cloud1 = document.querySelector('#dog-cloud1');
 let cloud2 = document.querySelector('#dog-cloud2');
 
-let cloudAnimationX = cloud1.animate(AnimationsKeyframes.cloudXKeyframes,{
-  duration: 20000,
+let cloudAnimationXOptions = {
+  duration: 40000,
   iterations: Infinity
-});
+};
 
-let cloudAnimationY = cloud1.animate(AnimationsKeyframes.cloudYKeyframes, {
+let cloudAnimationYOptions = {
   duration: 2000,
   iterations: Infinity
-});
+};
 
-let cloud2AnimationX = cloud2.animate(AnimationsKeyframes.cloud2XKeyframes,{
-  duration: 20000,
-  iterations: Infinity  
-});
+let cloudAnimationX = cloud1.animate([
+  { transform: 'translateX(0)' },
+  { transform: 'translateX(95%)' },
+  { transform: 'translateX(0)' }
+], cloudAnimationXOptions);
 
+let cloudAnimationY = cloud1.animate([
+  { marginTop: 0 },
+  { marginTop: '2px' },
+  { marginTop: 0 }
+], cloudAnimationYOptions);
+
+let cloud2AnimationX = cloud2.animate([
+ { transform: 'translateX(95%)' },
+ { transform: 'translateX(0)' },
+ { transform: 'translateX(95%)' }
+], cloudAnimationXOptions);
+
+
+// eyes animations
+let hideEyesKeyframes = [
+  { opacity: '1' },
+  { opacity: '0' }
+];
+let showEyesKeyframes = [
+  { opacity: '0' },
+  { opacity: '1' }
+];
+
+let eyesAnimationOptions = {
+  duration: 500,
+  fill: 'forwards'
+};
+
+function handleSnoopRest() {
+  if (isSnoopAwake()) {
+    snoopSleepingAnimation();
+  } else {
+    snoopWakeupAnimation();
+  }
+}
 
 function isSnoopAwake() {
   return snoopTailAnimation.playState !== 'paused';
 }
 
 function snoopSleepingAnimation() {
-  if (isSnoopAwake()) {
-    sleepyEyes.animate(AnimationsKeyframes.wakeupKeyframes, {
-      duration: 500,
-      fill: 'forwards'
-    });
-
-    awakeEyes.animate(AnimationsKeyframes.sleepKeyframes, {
-      duration: 500,
-      fill: 'forwards',
-    });
-
-    snoopTailAnimation.pause();
-  }
+  awakeEyes.animate(hideEyesKeyframes, eyesAnimationOptions);
+  sleepyEyes.animate(showEyesKeyframes, eyesAnimationOptions);
+  snoopTailAnimation.pause();
 };
 
 function snoopWakeupAnimation() {
-  if (!isSnoopAwake()) {  
-    sleepyEyes.animate(AnimationsKeyframes.sleepKeyframes, {
-      duration: 500,
-      fill: 'forwards'
-    });
-
-    awakeEyes.animate(AnimationsKeyframes.wakeupKeyframes, {
-      duration: 500,
-      fill: 'forwards',
-    });
-    
-    snoopTailAnimation.play();
-  }
+  sleepyEyes.animate(hideEyesKeyframes, eyesAnimationOptions);
+  awakeEyes.animate(showEyesKeyframes, eyesAnimationOptions);
+  snoopTailAnimation.play();
 };
